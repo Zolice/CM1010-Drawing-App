73477bcfb5e4837336d6079c25aadf202aef2c4d
Zolice <vividzhenlong@gmail.com>
2023-07-12 12:33:10 +0800

    Added hover details to tools
    Description of the tools will be displayed when mouse hovers over the tool if applicable

--- js/toolbox.js
+++ js/toolbox.js
@@ -6,7 +6,7 @@ function Toolbox() {
 	this.tools = [];
 	this.selectedTool = null;
 
-	var toolbarItemClick = function() {
+	var toolbarItemClick = function () {
 		//remove any existing borders
 		var items = selectAll(".toolButton");
 		for (var i = 0; i < items.length; i++) {
@@ -23,24 +23,23 @@ function Toolbox() {
 	}
 
 	//add a new tool icon to the html page
-	var addToolIcon = function(icon, name) {
+	var addToolIcon = function (icon, name, title = "") {
 		var sideBarItem = createDiv("<img src='" + icon + "'></div>");
 		sideBarItem.class('toolButton')
 		sideBarItem.id(name + "ToolButton")
 		sideBarItem.parent('sidebar');
 		sideBarItem.mouseClicked(toolbarItemClick);
-
-
+		sideBarItem.attribute("title", title)
 	};
 
 	//add a tool to the tools array
-	this.addTool = function(tool) {
+	this.addTool = function (tool) {
 		//check that the object tool has an icon and a name
 		if (!tool.hasOwnProperty("icon") || !tool.hasOwnProperty("name")) {
 			alert("make sure your tool has both a name and an icon");
 		}
 		this.tools.push(tool);
-		addToolIcon(tool.icon, tool.name);
+		addToolIcon(tool.icon, tool.name, tool.description ? tool.description : "");
 		//if no tool is selected (ie. none have been added so far)
 		//make this tool the selected one.
 		if (this.selectedTool == null) {
@@ -48,14 +47,14 @@ function Toolbox() {
 		}
 	};
 
-	this.selectTool = function(toolName) {
+	this.selectTool = function (toolName) {
 		//search through the tools for one that's name matches
 		//toolName
 		for (var i = 0; i < this.tools.length; i++) {
 			if (this.tools[i].name == toolName) {
 				//if the tool has an unselectTool method run it.
 				if (this.selectedTool != null && this.selectedTool.hasOwnProperty(
-						"unselectTool")) {
+					"unselectTool")) {
 					this.selectedTool.unselectTool();
 				}
 				//select the tool and highlight it on the toolbar
