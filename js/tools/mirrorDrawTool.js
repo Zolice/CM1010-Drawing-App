b8d446a87318209ee3d24c2d08546ae2f78189d8
Yong Zhen Long <vividzhenlong@gmail.com>
2023-08-14 15:58:30 +0800

    Updated SprayCanTool and MirrorDrawTool

--- js/tools/mirrorDrawTool.js
+++ js/tools/mirrorDrawTool.js
@@ -1,164 +1,175 @@
-function mirrorDrawTool() {
-	this.name = "mirrorDraw";
-	this.icon = "assets/mirrorDraw.png";
+class mirrorDrawTool extends Tool {
+    constructor() {
+        super()
 
-	//which axis is being mirrored (x or y) x is default
-	this.axis = "x";
-	//line of symmetry is halfway across the screen
-	this.lineOfSymmetry = width / 2;
+        this.name = "mirrorDraw";
+        this.icon = "assets/mirrorDraw.png";
+        this.description = "Draw on the canvas with your mouse, mirrored on the other side of the canvas";
 
-	//this changes in the jquery click handler. So storing it as
-	//a variable self now means we can still access it in the handler
-	var self = this;
+        this.initialize()
+    }
 
-	//where was the mouse on the last time draw was called.
-	//set it to -1 to begin with
-	var previousMouseX = -1;
-	var previousMouseY = -1;
+    initialize() {
+        // Initialize Values
+        //which axis is being mirrored (x or y) x is default
+        this.axis = "x";
+        //line of symmetry is halfway across the screen
+        this.lineOfSymmetry = width / 2;
 
-	//mouse coordinates for the other side of the Line of symmetry.
-	var previousOppositeMouseX = -1;
-	var previousOppositeMouseY = -1;
+        //where was the mouse on the last time draw was called.
+        //set it to -1 to begin with
+        this.previousMouseX = -1;
+        this.previousMouseY = -1;
 
-	this.draw = function () {
-		//display the last save state of pixels
-		updatePixels();
+        //mouse coordinates for the other side of the Line of symmetry.
+        this.previousOppositeMouseX = -1;
+        this.previousOppositeMouseY = -1;
 
-		//do the drawing if the mouse is pressed
-		if (mouseIsPressed && mouseInBounds()) {
-			//if the previous values are -1 set them to the current mouse location
-			//and mirrored positions
-			if (previousMouseX == -1) {
-				previousMouseX = mouseX;
-				previousMouseY = mouseY;
-				previousOppositeMouseX = this.calculateOpposite(mouseX, "x");
-				previousOppositeMouseY = this.calculateOpposite(mouseY, "y");
-			}
+        // This is used to provide compatibility for classes to work with sketch.js and toolbox.js
+        // Set draw() to this.draw
+        this.draw = () => { this.drawing() }
+        this.unselectTool = () => { this.reset() }
+        this.populateOptions = () => { this.populateOption() }
+    }
 
-			//if there are values in the previous locations
-			//draw a line between them and the current positions
-			else {
-				colourP.setStroke()
-				line(previousMouseX, previousMouseY, mouseX, mouseY);
-				previousMouseX = mouseX;
-				previousMouseY = mouseY;
+    drawing() {
+        //display the last save state of pixels
+        updatePixels();
 
-				//these are for the mirrored drawing the other side of the
-				//line of symmetry
-				var oX = this.calculateOpposite(mouseX, "x");
-				var oY = this.calculateOpposite(mouseY, "y");
-				line(previousOppositeMouseX, previousOppositeMouseY, oX, oY);
-				previousOppositeMouseX = oX;
-				previousOppositeMouseY = oY;
-			}
-		}
-		//if the mouse isn't pressed reset the previous values to -1
-		else {
-			previousMouseX = -1;
-			previousMouseY = -1;
+        //do the drawing if the mouse is pressed
+        if (mouseIsPressed && mouseInBounds()) {
+            //if the previous values are -1 set them to the current mouse location
+            //and mirrored positions
+            if (this.previousMouseX == -1) {
+                this.previousMouseX = mouseX;
+                this.previousMouseY = mouseY;
+                this.previousOppositeMouseX = this.calculateOpposite(mouseX, "x");
+                this.previousOppositeMouseY = this.calculateOpposite(mouseY, "y");
+            }
 
-			previousOppositeMouseX = -1;
-			previousOppositeMouseY = -1;
-		}
+            //if there are values in the previous locations
+            //draw a line between them and the current positions
+            else {
+                colourP.setStroke()
+                line(this.previousMouseX, this.previousMouseY, mouseX, mouseY);
+                this.previousMouseX = mouseX;
+                this.previousMouseY = mouseY;
 
-		//after the drawing is done save the pixel state. We don't want the
-		//line of symmetry to be part of our drawing
+                //these are for the mirrored drawing the other side of the
+                //line of symmetry
+                var oX = this.calculateOpposite(mouseX, "x");
+                var oY = this.calculateOpposite(mouseY, "y");
+                line(this.previousOppositeMouseX, this.previousOppositeMouseY, oX, oY);
+                this.previousOppositeMouseX = oX;
+                this.previousOppositeMouseY = oY;
+            }
+        }
+        //if the mouse isn't pressed reset the previous values to -1
+        else {
+            this.previousMouseX = -1;
+            this.previousMouseY = -1;
 
-		loadPixels();
+            this.previousOppositeMouseX = -1;
+            this.previousOppositeMouseY = -1;
+        }
 
-		//push the drawing state so that we can set the stroke weight and colour
-		push();
-		strokeWeight(3);
-		stroke("red");
-		//draw the line of symmetry
-		if (this.axis == "x") {
-			line(width / 2, 0, width / 2, height);
-		} else {
-			line(0, height / 2, width, height / 2);
-		}
-		//return to the original stroke
-		pop();
+        //after the drawing is done save the pixel state. We don't want the
+        //line of symmetry to be part of our drawing
 
-	};
+        loadPixels();
 
-	/*calculate an opposite coordinate the other side of the
-	 *symmetry line.
-	 *@param n number: location for either x or y coordinate
-	 *@param a [x,y]: the axis of the coordinate (y or y)
-	 *@return number: the opposite coordinate
-	 */
-	this.calculateOpposite = function (n, a) {
-		//if the axis isn't the one being mirrored return the same
-		//value
-		if (a != this.axis) {
-			return n;
-		}
+        //push the drawing state so that we can set the stroke weight and colour
+        push();
+        strokeWeight(3);
+        stroke("red");
+        //draw the line of symmetry
+        if (this.axis == "x") {
+            line(width / 2, 0, width / 2, height);
+        } else {
+            line(0, height / 2, width, height / 2);
+        }
+        //return to the original stroke
+        pop();
 
-		//if n is less than the line of symmetry return a coorindate
-		//that is far greater than the line of symmetry by the distance from
-		//n to that line.
-		if (n < this.lineOfSymmetry) {
-			return this.lineOfSymmetry + (this.lineOfSymmetry - n);
-		}
+    }
 
-		//otherwise a coordinate that is smaller than the line of symmetry
-		//by the distance between it and n.
-		else {
-			return this.lineOfSymmetry - (n - this.lineOfSymmetry);
-		}
-	};
+    /*calculate an opposite coordinate the other side of the
+     *symmetry line.
+     *@param n number: location for either x or y coordinate
+     *@param a [x,y]: the axis of the coordinate (y or y)
+     *@return number: the opposite coordinate
+     */
+    calculateOpposite(n, a) {
+        //if the axis isn't the one being mirrored return the same
+        //value
+        if (a != this.axis) {
+            return n;
+        }
 
+        //if n is less than the line of symmetry return a coorindate
+        //that is far greater than the line of symmetry by the distance from
+        //n to that line.
+        if (n < this.lineOfSymmetry) {
+            return this.lineOfSymmetry + (this.lineOfSymmetry - n);
+        }
 
-	//when the tool is deselected update the pixels to just show the drawing and
-	//hide the line of symmetry. Also clear options
-	this.unselectTool = function () {
-		updatePixels();
-		//clear options
-		select("#footer").html("")
-	};
+        //otherwise a coordinate that is smaller than the line of symmetry
+        //by the distance between it and n.
+        else {
+            return this.lineOfSymmetry - (n - this.lineOfSymmetry);
+        }
+    }
 
-	//adds a button and click handler to the options area. When clicked
-	//toggle the line of symmetry between horizonatl to vertical
-	this.populateOptions = function () {
-		// Create a button element
-		let button = document.createElement('div')
-		button.id = 'directionButton'
-		button.className = 'toolOptionsWrapper toolOptionsButton'
+    //when the tool is deselected update the pixels to just show the drawing and
+    //hide the line of symmetry. Also clear options
+    unselectTool() {
+        updatePixels();
+        //clear options
+        select("#footer").html("")
+    }
 
-		// Create a text element
-		let text = document.createElement('h5')
-		text.className = 'toolOptionsSetting toolOptionsText'
-		text.id = 'mirrorDrawToolDirection'
-		text.innerHTML = 'Make Horizontal'
+    //adds a button and click handler to the options area. When clicked
+    //toggle the line of symmetry between horizonatl to vertical
+    populateOption() {
+        // Create a button element
+        let button = document.createElement('div')
+        button.id = 'directionButton'
+        button.className = 'toolOptionsWrapper toolOptionsButton'
 
-		// Append text to button
-		button.appendChild(text)
+        // Create a text element
+        let text = document.createElement('h5')
+        text.className = 'toolOptionsSetting toolOptionsText'
+        text.id = 'mirrorDrawToolDirection'
+        text.innerHTML = 'Make Horizontal'
 
-		// Add button click event listener
-		button.onclick = () => {
-			let button = select("#mirrorDrawToolDirection");
+        // Append text to button
+        button.appendChild(text)
 
-			// Change the orientation of the tool
-			if (self.axis == "x") {
-				self.axis = "y";
-				self.lineOfSymmetry = height / 2;
-				button.html('Make Vertical');
-			} else {
-				self.axis = "x";
-				self.lineOfSymmetry = width / 2;
-				button.html('Make Horizontal');
-			}
-		}
+        // Add button click event listener
+        button.onclick = () => {
+            let button = select("#mirrorDrawToolDirection");
 
-		// Empty the footer and append button
-		select("#footer").html("")
-		select("#footer").elt.appendChild(button)
+            // Change the orientation of the tool
+            if (this.axis == "x") {
+                this.axis = "y";
+                this.lineOfSymmetry = height / 2;
+                button.html('Make Vertical');
+            } else {
+                this.axis = "x";
+                this.lineOfSymmetry = width / 2;
+                button.html('Make Horizontal');
+            }
+        }
 
-		// Actvate the tool
-		self.axis = "x";
-		self.lineOfSymmetry = width / 2;
+        // Empty the footer and append button
+        select("#footer").html("")
+        select("#footer").elt.appendChild(button)
 
-		// Add stroke options to footer
-		colourP.loadColors(true, false)
-	};
+        // Actvate the tool
+        this.axis = "x";
+        this.lineOfSymmetry = width / 2;
+
+        // Add stroke options to footer
+        colourP.loadColors(true, false)
+    }
 }
--- js/tools/sprayCanTool.js
+++ js/tools/sprayCanTool.js
@@ -1,92 +1,107 @@
-function SprayCanTool() {
+class SprayCanTool extends Tool {
+    constructor() {
+        super()
 
-	this.name = "sprayCanTool";
-	this.icon = "assets/sprayCan.png";
+        this.name = "sprayCanTool";
+        this.icon = "assets/sprayCan.png";
+        this.description = "Spray paint on the canvas with your mouse";
 
-	this.count = 13;
-	this.spread = 10;
+        this.initialize()
+    }
 
-	this.draw = function () {
-		var r = random(5, 10);
-		if (mouseIsPressed && mouseInBounds()) {
-			for (var i = 0; i < this.count; i++) {
-				colourP.setStroke()
-				point(random(mouseX - this.spread, mouseX + this.spread), random(mouseY - this.spread, mouseY + this.spread));
-			}
-		}
-	};
+    initialize() {
+        // Initialize Values
+        this.count = 13;
+        this.spread = 10;
 
-	this.populateOptions = function () {
-		// Empty the footer's current options
-		select("#footer").html("")
+        // This is used to provide compatibility for classes to work with sketch.js and toolbox.js
+        // Set draw() to this.draw
+        this.draw = () => { this.drawing() }
+        this.unselectTool = () => { this.reset() }
+        this.populateOptions = () => { this.populateOption() }
+    }
 
-		// Add Spread options to footer
-		// Create a text element to indicate Spread
-		let spreadText = document.createElement("h5")
-		spreadText.className = "toolOptionsType toolOptionsText"
-		spreadText.innerHTML = "Spread"
+    drawing() {
+        var r = random(5, 10);
+        if (mouseIsPressed && mouseInBounds()) {
+            for (var i = 0; i < this.count; i++) {
+                colourP.setStroke()
+                point(random(mouseX - this.spread, mouseX + this.spread), random(mouseY - this.spread, mouseY + this.spread));
+            }
+        }
+    }
 
-		// Append text element to footer
-		select("#footer").elt.appendChild(spreadText)
+    populateOption() {
+        // Empty the footer's current options
+        select("#footer").html("")
 
-		// Create a Div to hold the spread options
-		let spreadDiv = document.createElement("div")
-		spreadDiv.className = "toolOptionsWrapper"
+        // Add Spread options to footer
+        // Create a text element to indicate Spread
+        let spreadText = document.createElement("h5")
+        spreadText.className = "toolOptionsType toolOptionsText"
+        spreadText.innerHTML = "Spread"
 
-		// Create a number input element to hold the spread Size
-		let spreadSizeInput = document.createElement("input")
-		spreadSizeInput.className = "textSelector"
-		spreadSizeInput.type = "number"
-		spreadSizeInput.id = "spreadSize"
-		spreadSizeInput.value = this.spread
-		spreadSizeInput.step = 0.1
+        // Append text element to footer
+        select("#footer").elt.appendChild(spreadText)
 
-		// Call the callback function when the input is changed
-		spreadSizeInput.oninput = () => { 
-			// Update the value
-			this.spread = spreadSizeInput.valueAsNumber
-		}
+        // Create a Div to hold the spread options
+        let spreadDiv = document.createElement("div")
+        spreadDiv.className = "toolOptionsWrapper"
 
-		// Add the number input element to the spread divider
-		spreadDiv.appendChild(spreadSizeInput)
+        // Create a number input element to hold the spread Size
+        let spreadSizeInput = document.createElement("input")
+        spreadSizeInput.className = "textSelector"
+        spreadSizeInput.type = "number"
+        spreadSizeInput.id = "spreadSize"
+        spreadSizeInput.value = this.spread
+        spreadSizeInput.step = 0.1
 
-		// Add the spread divider to the footer
-		select("#footer").elt.appendChild(spreadDiv)
+        // Call the callback function when the input is changed
+        spreadSizeInput.oninput = () => {
+            // Update the value
+            this.spread = spreadSizeInput.valueAsNumber
+        }
 
-		// Add count options to footer
-		// Create a text element to indicate count
-		let countText = document.createElement("h5")
-		countText.className = "toolOptionsType toolOptionsText"
-		countText.innerHTML = "Count"
+        // Add the number input element to the spread divider
+        spreadDiv.appendChild(spreadSizeInput)
 
-		// Append text element to footer
-		select("#footer").elt.appendChild(countText)
+        // Add the spread divider to the footer
+        select("#footer").elt.appendChild(spreadDiv)
 
-		// Create a Div to hold the count options
-		let countDiv = document.createElement("div")
-		countDiv.className = "toolOptionsWrapper"
+        // Add count options to footer
+        // Create a text element to indicate count
+        let countText = document.createElement("h5")
+        countText.className = "toolOptionsType toolOptionsText"
+        countText.innerHTML = "Count"
 
-		// Create a number input element to hold the count Size
-		let countSizeInput = document.createElement("input")
-		countSizeInput.className = "textSelector"
-		countSizeInput.type = "number"
-		countSizeInput.id = "countSize"
-		countSizeInput.value = this.count
-		countSizeInput.step = 0.1
+        // Append text element to footer
+        select("#footer").elt.appendChild(countText)
 
-		// Call the callback function when the input is changed
-		countSizeInput.oninput = () => { 
-			// Update the value
-			this.count = countSizeInput.valueAsNumber
-		}
+        // Create a Div to hold the count options
+        let countDiv = document.createElement("div")
+        countDiv.className = "toolOptionsWrapper"
 
-		// Add the number input element to the count divider
-		countDiv.appendChild(countSizeInput)
+        // Create a number input element to hold the count Size
+        let countSizeInput = document.createElement("input")
+        countSizeInput.className = "textSelector"
+        countSizeInput.type = "number"
+        countSizeInput.id = "countSize"
+        countSizeInput.value = this.count
+        countSizeInput.step = 0.1
 
-		// Add the count divider to the footer
-		select("#footer").elt.appendChild(countDiv)
+        // Call the callback function when the input is changed
+        countSizeInput.oninput = () => {
+            // Update the value
+            this.count = countSizeInput.valueAsNumber
+        }
 
-		// Add spread options to footer
-		colourP.loadColors(true, false)
-	}
+        // Add the number input element to the count divider
+        countDiv.appendChild(countSizeInput)
+
+        // Add the count divider to the footer
+        select("#footer").elt.appendChild(countDiv)
+
+        // Add spread options to footer
+        colourP.loadColors(true, false)
+    }
 }
